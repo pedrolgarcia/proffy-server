5195d0aa608dab0b4e8d21001ac8e4aecc8ff6f4
Pedro Garcia <pedrolopes.info@gmai.com>
2020-08-09 00:13:26 -0300

    fix: List classes query

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -25,20 +25,25 @@ export default class ClassesController {
 
     const timeInMinutes = convertHourToMinutes(time);
 
-    const classes = await db("classes")
-      .whereExists(function () {
-        this.select("class_schedule.*")
-          .from("class_schedule")
-          .whereRaw("`class_schedule`.`class_id` = `classes`.`id`")
-          .whereRaw("`class_schedule`.`week_day` = ??", [Number(week_day)])
-          .whereRaw("`class_schedule`.`from` <= ??", [timeInMinutes])
-          .whereRaw("`class_schedule`.`to` > ??", [timeInMinutes]);
-      })
-      .where("classes.subject", "=", subject)
-      .join("users", "classes.user_id", "=", "user_id")
-      .select(["classes.*", "users.*"]);
+    try {
+      const classes = await db("classes")
+        .whereExists(function () {
+          this.select("class_schedule.*")
+            .from("class_schedule")
+            .whereRaw("`class_schedule`.`class_id` = `classes`.`id`")
+            .whereRaw("`class_schedule`.`week_day` = ??", [Number(week_day)])
+            .whereRaw("`class_schedule`.`from` <= ??", [timeInMinutes])
+            .whereRaw("`class_schedule`.`to` > ??", [timeInMinutes]);
+        })
+        .where("classes.subject", "=", subject)
+        .join("users", "classes.user_id", "=", "users.id")
+        .select(["classes.*", "users.*"]);
 
-    return response.json(classes);
+      return response.json(classes);
+    } catch (err) {
+      console.log(err);
+      return response.json({ error: "Erro ao buscar" });
+    }
   }
 
   async create(request: Request, response: Response) {
